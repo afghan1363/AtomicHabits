d8022044c43a208d393422729965ae756805c9e3
Maxim Abavi <afghan1363@ya.ru>
2024-02-16 21:41:27 +0500

    flake8

--- users/management/commands/create_simple_user.py
+++ users/management/commands/create_simple_user.py
@@ -5,18 +5,18 @@ from users.models import User
 class Command(BaseCommand):
     def handle(self, *args, **options):
         email = input('email: ')
+        chat_id = input('chat_id: ')
         first_name = input('first_name: ')
         last_name = input('last_name: ')
         password = 'SkyPro123'
-        is_moderator = bool(int(input('is_moderator: 1 - True, 0 - False: ')))
         user = User.objects.create(
             email=email,
+            chat_id=chat_id,
             first_name=first_name,
             last_name=last_name,
             is_superuser=False,
             is_staff=False,
             is_active=True,
-            is_moderator=is_moderator
         )
         user.set_password(password)
         user.save()
--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -5,11 +5,13 @@ from users.models import User
 class Command(BaseCommand):
     def handle(self, *args, **options):
         email = input('email: ')
+        chat_id = input('chat_id: ')
         first_name = input('first_name: ')
         last_name = input('last_name: ')
         password = input('password: ')
         user = User.objects.create(
             email=email,
+            chat_id=chat_id,
             first_name=first_name,
             last_name=last_name,
             is_superuser=True,
